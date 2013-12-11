dcd73d4a62ad674b3c04cd6edd9cedfd901cb9de
Daisuke Maki <lestrrat+github@gmail.com>
2013-12-12 08:26:24 +0900

    grr

--- src/stf/queue.go
+++ src/stf/queue.go
@@ -120,7 +120,7 @@ func (self *QueueApi) Dequeue (queueName string, timeout int) (*WorkerArg, error
   }
 
   err := errors.New("Could not dequeue anything")
-  self.Debugf("%s", err)
+  ctx.Debugf("%s", err)
   // if we got here, we were not able to dequeue anything
   return nil, err
 }
