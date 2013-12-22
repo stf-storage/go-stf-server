efc821cb5fb62de2ec4364bf76153eaf9ec44b75
Daisuke Maki <lestrrat+github@gmail.com>
2013-12-22 20:20:07 +0900

    Add the dirname of the worker executable to PATH so commands can be found more easily

--- src/cli/worker.go
+++ src/cli/worker.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+  "log"
+  "os"
+  "path/filepath"
   "stf/worker"
+  "strings"
 )
 func main() {
+  // Get our path, and add this to PATH, so other binaries
+  // can safely be executed
+  dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+  if err != nil {
+    log.Fatalf("Failed to find our directory name?!: %s", err)
+  }
+
+  path := os.Getenv("PATH")
+
+  os.Setenv("PATH", strings.Join([]string { path, dir }, string(os.PathListSeparator) ))
+
   drone := worker.NewDroneFromArgv()
   drone.Start()
 }
