23d1a990d29704f6a2a357eaf8ed415071529c46
Daisuke Maki <lestrrat+github@gmail.com>
2013-12-24 20:13:38 +0900

    Add more tests

--- src/stf/stf_test.go
+++ src/stf/stf_test.go
@@ -3,71 +3,161 @@ package stf
 import (
   "fmt"
   "io/ioutil"
+  "log"
   "os"
+  "os/exec"
+  "path/filepath"
   "net/http"
   "net/http/httptest"
   "runtime"
+  "syscall"
   "testing"
 )
 
 type TestEnv struct {
+  Test        *testing.T
+  Guards      []func()
   WorkDir     string
   ConfigFile  *os.File
+  Database    *TestDatabase
 }
 
-func setupEnvironment (t *testing.T) (*TestEnv, func ()) {
+type TestDatabase struct {
+  Config  *DatabaseConfig
+  Socket  string
+  DataDir string
+  PidFile string
+  TmpDir  string
+}
+
+func NewTestEnv (t *testing.T) (*TestEnv) {
   env := &TestEnv {}
-  workdir, workdirGuard := createTemporaryDir(t)
-  configfile, configfileGuard := createTemporaryConfig(t, workdir)
+  env.Test = t
+  return env
+}
 
-  env.WorkDir = workdir
-  env.ConfigFile = configfile
+func (self *TestEnv) Setup () {
+  self.createTemporaryDir()
+  self.startDatabase()
+  self.createTemporaryConfig()
+}
 
-  guard := func() {
-    configfileGuard()
-    workdirGuard()
+func (self *TestEnv) Release () {
+  for _, f := range self.Guards {
+    f()
+  }
+}
+
+func (self *TestEnv) Errorf(format string, args ...interface {}) {
+  self.Test.Errorf(format, args...)
+}
+
+func (self *TestEnv) Logf(format string, args ...interface {}) {
+  self.Test.Logf(format, args...)
+}
+
+func (self *TestEnv) startDatabase()  {
+  fullpath, err := exec.LookPath("mysqld")
+  if err != nil {
+    log.Fatalf("Failed to find mysqld in path: %s", err)
   }
 
-  return env, guard
+  basedir := filepath.Join(self.WorkDir, "mysql")
+  cmd := exec.Command(
+    fullpath,
+    fmt.Sprintf("--defaults-file=%s", basedir),
+  )
+  if err != nil {
+    log.Fatalf("Failed to execute command %s: %s", cmd, err)
+  }
+
+  tmpdir := filepath.Join(basedir, "tmp")
+  td := &TestDatabase {
+    nil,
+    filepath.Join(tmpdir, "mysql.sock"), // Socket
+    filepath.Join(basedir, "var"), // DataDir
+    filepath.Join(tmpdir, "mysqld.pid"), // PidFile
+    tmpdir,
+  }
+
+  // td.WriteMycnf(filepath.Join(basedir, "etc", "my.cnf"))
+
+  config := &DatabaseConfig {
+    "mysql",
+    "root",
+    "",
+    fmt.Sprintf("unix(%s)", td.Socket),
+    "test",
+  }
+
+  td.Config = config
+  self.Database = td
+
+  self.Logf("Started database listening at %s", td.Socket)
+
+  self.Guards = append(self.Guards, func() {
+    if cmd.Process != nil {
+      cmd.Process.Signal(syscall.SIGTERM)
+    }
+  })
 }
 
-func createTemporaryDir (t *testing.T) (string, func ()) {
+func (self *TestEnv) createTemporaryDir() {
   tempdir, err := ioutil.TempDir("", "stf-test");
   if err != nil {
-    t.Errorf("Failed to create a temporary directory: %s", err)
+    self.Errorf("Failed to create a temporary directory: %s", err)
   }
-  return tempdir, func() {
-    t.Logf("Releasing work dir %s", tempdir)
+
+  self.WorkDir = tempdir
+  self.Guards  = append(self.Guards, func() {
+    self.Logf("Releasing work dir %s", tempdir)
     os.RemoveAll(tempdir)
-  }
+  })
 }
 
-func createTemporaryConfig(t *testing.T, dir string) (*os.File, func()) {
-  tempfile, err := ioutil.TempFile(dir, "test.gcfg")
+func (self *TestEnv) createTemporaryConfig() {
+  tempfile, err := ioutil.TempFile(self.WorkDir, "test.gcfg")
   if err != nil {
-    t.Errorf("Failed to create tempfile: %s", err)
+    self.Errorf("Failed to create tempfile: %s", err)
   }
 
-  t.Logf("Created config file %s", tempfile.Name())
+  self.Logf("Created config file %s", tempfile.Name())
 
-  return tempfile, func () {
-    t.Logf("Removing config file %s", tempfile.Name())
+  self.ConfigFile = tempfile
+  self.Guards = append(self.Guards, func() {
+    self.Logf("Removing config file %s", tempfile.Name())
     os.Remove(tempfile.Name())
-  }
+  })
 }
 
-func startTemporaryStorageServer() func() {
+func (self *TestEnv) startTemporaryStorageServer(dir string) {
+  ss := NewStorageServer("dummy", dir)
+  dts := httptest.NewServer(ss)
+
+  // Register ourselves in the database
+  db, err := ConnectDB(self.Database.Config)
+  if err != nil {
+    log.Fatalf("Failed to connect to database: %s", err)
+  }
+  db.Exec("INSERT INTO storage (uri, mode) VALUES (?, 1)", dts.URL)
+
+  self.Guards = append(self.Guards, func() {
+    dts.Close()
+  })
+}
+
+func (self *TestEnv) startTemporaryStorageServers() {
   for i := 1; i <= 3; i++ {
-    dir := 
-  dts := httptest.NewServer(d)
-
-
-  return func() {}
+    mydir := filepath.Join(self.WorkDir, fmt.Sprintf("storage%03d", i))
+    self.startTemporaryStorageServer(mydir)
+  }
 }
 
 func TestBasic(t *testing.T) {
-  env, guard := setupEnvironment(t)
-  defer guard()
+  env := NewTestEnv(t)
+  defer env.Release()
+
+  env.Setup()
 
   os.Setenv("STF_CONFIG", env.ConfigFile.Name())
   ctx, err := BootstrapContext()
