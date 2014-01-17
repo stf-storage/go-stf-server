9289dbc2909bad8cde49cdd51f6d84f0f0170ee1
Daisuke Maki <lestrrat+github@gmail.com>
2014-01-17 09:16:50 +0900

    set STF_QUEUE_TYPE

--- test.sh
+++ test.sh
@@ -2,5 +2,10 @@
 
 export SKIP_DEPS=1
 sh ./build.sh
+
+if [ -z "$STF_QUEUE_TYPE" ]; then
+  STF_QUEUE_TYPE=q4m
+fi
+
 export STF_DEBUG="1:`pwd`/test.log"
-exec go test -v .
+exec go test -tags $STF_QUEUE_TYPE -v .
