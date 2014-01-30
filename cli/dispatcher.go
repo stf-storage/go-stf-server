fa241a147f5f1bf6e29ad66a6aeafa9c3eece3da
Daisuke Maki <lestrrat+github@gmail.com>
2014-01-30 13:40:47 +0900

    Allow passing --id on dispatcher binary

--- cli/dispatcher.go
+++ cli/dispatcher.go
@@ -16,8 +16,15 @@ func main() {
     log.Fatalf("Could not determine current working directory")
   }
 
+  var dispatcherId uint64
   defaultConfig := path.Join(pwd, "etc", "config.gcfg")
 
+  flag.Uint64Var(
+    &dispatcherId,
+    "id",
+    0,
+    "Dispatcher ID, overrides config file settings",
+  )
   flag.StringVar(
     &configFile,
     "config",
@@ -30,6 +37,10 @@ func main() {
   config, err := stf.BootstrapConfig()
   if err != nil {
     log.Fatal(err)
+  }
+
+  if dispatcherId > 0 {
+    config.Dispatcher.ServerId = dispatcherId
   }
 
   d := stf.NewDispatcher(config)
